0b3cc5a63d8156680a88a79175e2b86ab440558f
Yury Dyumkin <y.dyumkin@itransition.com>
2014-10-17 17:45:04 +0300

    rbac

--- common/config/main.php
+++ common/config/main.php
@@ -9,6 +9,18 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+        ],
+
+        'authManager' => [
+            'class' => \Zelenin\yii\modules\Rbac\components\DbManager::className(),
+            'itemFile' => '@common/config/rbac/items.php',
+            'assignmentFile' => '@common/config/rbac/assignments.php',
+            'ruleFile' => '@common/config/rbac/rules.php',
+            'defaultRole' => 'user',
+            'roleParam' => 'role', // User model attribute
+            // optional
+            'enableCaching' => false,
+            'cachingDuration' => 60
         ]
     ],
 ];
